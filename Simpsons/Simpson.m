bfa3d3c3b3ddd9af2e892d745b74f6955479343e
Kylecon15 <49926879+Kylecon15@users.noreply.github.com>
2019-05-08 23:03:39 -0600

    Rename Simpson.m to Simpsons/Simpson.m

--- Simpsons/Simpson.m
+++ Simpsons/Simpson.m
@@ -3,7 +3,7 @@ function I = Simpson(x,y)
 % x = equally or unequally spaced interval vector
 % y = the values of the function with respect to x
 % Outputs:
-% I = the integral of the vector of function values ‘y’ with respect to ‘x’
+% I = the integral of the vector of function values â€˜yâ€™ with respect to â€˜xâ€™
 % Warning: 
 % If the interval is odd then the trapezoidal rule must be used on the last interval 
 
